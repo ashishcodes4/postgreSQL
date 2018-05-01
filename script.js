adf641d8049aa9b9b41e9eaf7c0512fb90d1e484
Ashish Singh <ashishcodes4@gmail.com>
2018-05-01 22:06:31 +0530

    adding new route

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ app.put('/', (req, res)=> {
     res.send('this is the home page');
 });
 
+
+app.put('/', (req, res)=> {
+    res.send('this is the home page');
+});
+
 app.listen(3000, ()=>{
     console.log("server is listening on port 3000");
 });
